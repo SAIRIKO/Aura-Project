38da8db700e89d4a1c81641a564fad8f1f9e073b
Sairiko <murilotoddyn@gmail.com>
2025-11-20 22:30:16 -0300

    atualizção IP front-end

--- App/api.config.ts
+++ App/api.config.ts
@@ -1,6 +1,5 @@
 // URL público do seu backend rodando no Render:
-export const API_BASE_URL = "http://192.168.0.93:4000/api";
-
+export const API_BASE_URL = "https://aura-project-uowq.onrender.com/api";
 // Você pode adicionar endpoints comuns aqui
 // Exemplo:
 export const ENDPOINTS = {
